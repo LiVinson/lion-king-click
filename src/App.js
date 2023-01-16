3f4eb59fb6c483071e49f00cca30852d1c9400b5
Lisa Vinson <lisa@whitewhale.net>
2023-01-16 15:38:28 -0500

    refactored App class component as function with useState

--- src/App.js
+++ src/App.js
@@ -1,82 +1,102 @@
-import React, { Component } from "react"
+import { useState, useEffect } from "react"
 import Navbar from "./components/Navbar"
 import Instructions from "./components/Instructions"
 import Wrapper from "./components/Wrapper"
 import ImageCard from "./components/ImageCard"
 import Footer from "./components/Footer"
 
-const clickItems = [
+const initialImages = [
   {
     id: 0,
     alt: "hyenas",
-    img_url: `${process.env.PUBLIC_URL}/images/hyenas.jpg`
+    img_url: `${process.env.PUBLIC_URL}/images/hyenas.jpg`,
+    clicked: false
   },
   {
     id: 1,
     alt: "mufasa",
-    img_url: `${process.env.PUBLIC_URL}/images/mufasa.jpg`
+    img_url: `${process.env.PUBLIC_URL}/images/mufasa.jpg`,
+    clicked: false
   },
   {
     id: 2,
     alt: "adult nala",
-    img_url: `${process.env.PUBLIC_URL}/images/nala_adult.jpg`
+    img_url: `${process.env.PUBLIC_URL}/images/nala_adult.jpg`,
+    clicked: false
   },
   {
     id: 3,
     alt: "young nala",
-    img_url: `${process.env.PUBLIC_URL}/images/nala_young.png`
+    img_url: `${process.env.PUBLIC_URL}/images/nala_young.png`,
+    clicked: false
   },
   {
     id: 4,
     alt: "pumbaa",
-    img_url: `${process.env.PUBLIC_URL}/images/pumbaa.png`
+    img_url: `${process.env.PUBLIC_URL}/images/pumbaa.png`,
+    clicked: false
   },
   {
     id: 5,
     alt: "rafiki",
-    img_url: `${process.env.PUBLIC_URL}/images/rafiki.jpg`
+    img_url: `${process.env.PUBLIC_URL}/images/rafiki.jpg`,
+    clicked: false
   },
   {
     id: 6,
     alt: "sarabi",
-    img_url: `${process.env.PUBLIC_URL}/images/sarabi.png`
+    img_url: `${process.env.PUBLIC_URL}/images/sarabi.png`,
+    clicked: false
   },
   {
     id: 7,
     alt: "scar",
-    img_url: `${process.env.PUBLIC_URL}/images/scar.jpeg`
+    img_url: `${process.env.PUBLIC_URL}/images/scar.jpeg`,
+    clicked: false
   },
   {
     id: 8,
     alt: "adult simba",
-    img_url: `${process.env.PUBLIC_URL}/images/simba_adult.jpg`
+    img_url: `${process.env.PUBLIC_URL}/images/simba_adult.jpg`,
+    clicked: false
   },
   {
     id: 9,
     alt: "young simba",
-    img_url: `${process.env.PUBLIC_URL}/images/simba_young.jpeg`
+    img_url: `${process.env.PUBLIC_URL}/images/simba_young.jpeg`,
+    clicked: false
   },
   {
     id: 10,
     alt: "timon",
-    img_url: `${process.env.PUBLIC_URL}/images/timon.png`
+    img_url: `${process.env.PUBLIC_URL}/images/timon.png`,
+    clicked: false
   },
   {
     id: 11,
     alt: "zazu",
-    img_url: `${process.env.PUBLIC_URL}/images/zazu.jpg`
+    img_url: `${process.env.PUBLIC_URL}/images/zazu.jpg`,
+    clicked: false
   }
 ]
 
-class App extends Component {
-  state = {
-    clickedStatus: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+
+
+function App() {
+
+
+  const [gameState, setGameState] = useState({
+    score: 0,
     topScore: 0,
     gameOver: false,
-    wonGame: false
-  }
+    gameWon: false,
+    clickedImages: initialImages,
+    selectedImageId: null
+  })
 
-  shuffleArray = imageArray => {
+  
+
+  const shuffleArray = imageArray => {
     //Shuffles the image array each time the App is rendered
     let arr = imageArray
     let currentIndex = arr.length
@@ -97,107 +117,91 @@ class App extends Component {
     return arr //Once all are shuffled, return array
   }
 
-  determineClickStatus = imageId => {
-    //arrow function allows this to represent the App object instance
 
-    if (this.state.clickedStatus[imageId]) {
-      this.lostGame()
-    } else {
-      //if image not previously clicked, update state at index of image to 1
-      let newArr = this.state.clickedStatus
-      newArr[imageId] = 1
 
-      let currentScore = this.calculateScore(this.state.clickedStatus)
-      let topScore = this.state.topScore
 
-      if (currentScore > topScore) {
-        topScore = currentScore
-      }
+  const handleImageClick = imageId => {
+    console.log(imageId)
+    console.log("click ", gameState.clickedImages.find(image => image.id === imageId).alt)
 
-      if (currentScore === 12) {
-        //If all images clicked, end the game
-        this.wonGame(newArr, topScore)
+      //If previously clicked image selected, lose game
+
+      if (gameState.clickedImages.some((image => image.id === imageId && image.clicked))) {
+        console.log("Lost");
+
+        const resetImages = gameState.clickedImages.map(image => {
+          image.clicked = false;
+          return image
+        })
+        setGameState({
+          ...gameState,
+          gameOver: true,
+          score: 0,
+          gameWon:false,
+          clickedImages: resetImages,
+        })
       } else {
-        this.setState({
-          //Continue playing
-          clickedImage: newArr,
-          topScore: topScore,
-          gameOver: false,
-          wonGame: false
+        console.log("correct")
+        //if image not previously clicked, update clicked to true
+        let newArr = gameState.clickedImages.map(image => image.id === imageId ? { ...image, clicked: true } : image)
+
+        setGameState({
+          ...gameState,
+          score: gameState.score + 1,
+          topScore: gameState.score + 1 >= gameState.topScore ? gameState.score + 1 : gameState.topScore,
+          clickedImages: newArr,
+          gameWon: gameState.score + 1 === gameState.clickedImages.length ? true : false,
+          gameOver: false
+
         })
       }
     }
-  }
 
-  calculateScore = arr => {
-    //Creates an array based on clicked status, and returns the length to calculate the score.
-    let clickedArr = arr.filter(image => image === 1)
-    return clickedArr.length
-  }
+  
 
-  lostGame = () => {
-    //Add logic to determine if won or loss
-    this.setState({
-      clickedStatus: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      gameOver: true
-    })
-  }
 
-  wonGame = (newArr, topScore) => {
-    this.setState({
-      clickedStatus: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      topScore: topScore,
-      wonGame: true
-    })
-  }
-  render = () => {
-    return (
-      <div>
-        <Navbar
-          currentScore={this.calculateScore(this.state.clickedStatus)}
-          topScore={this.state.topScore}
-          message={this.state.wonGame ? "" : "hide-text"}
-        />
-        <Instructions />
-        <Wrapper>
-          <div className="row">
-            {this.shuffleArray(clickItems).map(image => (
-              <ImageCard
-                key={image.id}
-                id={image.id}
-                imgURL={image.img_url}
-                onClick={this.determineClickStatus}
-                cardClass={
-                  this.state.gameOver ? "incorrect red-border card" : "card"
-                }
-              />
-            ))}
-          </div>
-        </Wrapper>
-        <Footer />
-      </div>
-    )
-  }
+
+
+
+
+  return (
+    <div>
+      <Navbar
+        currentScore={gameState.score}
+        topScore={gameState.topScore}
+        message={gameState.gameWon ? "" : "hide-text"}
+      />
+      <Instructions />
+      <Wrapper>
+        <div className="row">
+          {shuffleArray(gameState.clickedImages).map(image => (
+            <ImageCard
+              key={image.id}
+              id={image.id}
+              imgURL={image.img_url}
+              onClick={handleImageClick}
+              cardClass={gameState.gameOver ? "incorrect red-border card" : "card"}
+            />
+          ))}
+        </div>
+      </Wrapper>
+      <Footer />
+    </div>
+  )
+
 }
 
 export default App
 
-/* PseudoCode 
-  //ON load: 
-    Navbar with starting score and top score
-    Jumbotron with instructions
-    Wrapper with 12 unique images (4 * 3), displayed in random order
+/*
 
-  //On click of image:
-      If image was not previously clicked: 
-        - Increment the score
-        - If current score > top score, reset the top score to current score
-        - If score = max score: **
-        - Shuffle the order of 12 images
-        
-      If image was previously clicked:
-        - Shake the images?
-        - Reset current score to 0
-        - Reset all images to not clicked
-        - Shuffle the 12 images
+
+
+- On click of images
+  -If new game - reset gameOver
+
+- Determine game reset behavior 
+    - If starting a new game, reset gameOver, gameWon, score
+
+
 */
